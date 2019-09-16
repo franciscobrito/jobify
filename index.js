6d052d7104a956b68ebe59bb1541928edbd88be3
Francisco Brito <francisco.brito.ac@gmail.com>
2019-09-16 18:06:19 -0300

    Remove comment. Added port as envy variable.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -90,7 +92,7 @@ const init = async() => {
 
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err) {
         Console.log('Não foi possivel iniciar o servidor do Jobify')
     } else {
