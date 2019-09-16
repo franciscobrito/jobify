ad550ef288e046ec11992123959da7c293f7bd4a
Francisco Brito <francisco.brito.ac@gmail.com>
2019-09-16 18:37:14 -0300

    Fixing public.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async(request, response) =>{
